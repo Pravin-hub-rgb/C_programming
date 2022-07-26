60813a6e12bcfea099c2c27093a171cf3b3b81cd
Pravin-hub-rgb <64892563+Pravin-hub-rgb@users.noreply.github.com>
2022-07-26 16:58:31 +0530

    2 new practise program add

--- practiseProgram/program/multiplication1.c
+++ practiseProgram/program/multiplication1.c
@@ -4,7 +4,7 @@ int main()
     int num;
     printf("Enter the no. whose multiplication table you want :");
     scanf("%d", &num);
-    for (int i = 1; i < 11; i++)
+    for (int i = 1; i <= 10; i++)
     {
         printf("%d X %d = %d\n",num,i,num*i);
     }
